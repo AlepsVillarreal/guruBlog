da6d8e600f7370d120d64231f719c0a483e82860
Alejandro <posdata.5@hotmail.com>
2019-01-01 14:48:04 -0600

    Added the articles app and changed the config to reroute to the urls from the article app

--- pythonguru/urls.py
+++ pythonguru/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 #from django.contrib import admin
 #from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^articles/', include('articles.urls')),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
 ]
