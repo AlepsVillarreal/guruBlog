604abd32a500ef7c8eda4e871278bc6d0159e609
Alejandro <posdata.5@hotmail.com>
2019-01-01 13:30:21 -0600

    Added about page and home page

--- pythonguru/urls.py
+++ pythonguru/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#from django.contrib import admin
+#from django.urls import path
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^about/$', views.about),
+    url(r'^$', views.homepage),
 ]
