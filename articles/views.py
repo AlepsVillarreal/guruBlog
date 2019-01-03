65c7a7c8362545a8e6e2837e454510b78af18c49
Alejandro <posdata.5@hotmail.com>
2019-01-02 22:59:48 -0600

    Enabled named urls to be applied with the base layout. Also, new template for the individual article

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
-from django.http import HttpResponse
 
-# Create your views here.
 def article_list(request):
-	articles = Article.objects.all().order_by('date')
-	return render(request, 'articles/article_list.html', {'articles': articles})
+    articles = Article.objects.all().order_by('date');
+    return render(request, 'articles/article_list.html', { 'articles': articles })
 
 def article_detail(request, slug):
-	return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', { 'article': article })
