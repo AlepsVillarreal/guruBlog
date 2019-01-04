ca0858c84e56430ee9f14217784ddc14ab75ce52
Alejandro <posdata.5@hotmail.com>
2019-01-03 22:34:49 -0600

    Logout function in base_layout.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 def signup_view(request):
-	if request.method=='POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			#Log the user in
-			user = form.save()
-			login(request, user)
-			return redirect('articles:list')
-	elif request.method=='GET':
-	    form = UserCreationForm()
-	return render(request, 'accounts/signup.html', { 'form': form })
+    if request.method == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+             user = form.save()
+             #  log the user in
+             login(request, user)
+             return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
 
 def login_view(request):
-	if request.method == 'POST':
-		form = AuthenticationForm(data=request.POST)
-		if form.is_valid():
-			#login the user
-			user = form.get_user()
-			login(request, user)
-			return redirect('articles:list')
-	elif request.method == 'GET':
-		form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
 
-	return render(request, 'accounts/login.html', {'form':form})
+def logout_view(request):
+    if request.method == 'POST':
+            logout(request)
+            return redirect('articles:list')
