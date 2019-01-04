b3200137ebc88e3af8e28ea277185006b92af10a
Alejandro <posdata.5@hotmail.com>
2019-01-03 22:04:38 -0600

    Enabled saving of users using accounts.signup.html. Also wrapped home.html and about.html in the base layout block

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def signup_view(request):
-    form = UserCreationForm()
-    return render(request, 'accounts/signup.html', { 'form': form })
+	if request.method=='POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			#Log the user in
+			return redirect('articles:list')
+	elif request.method=='GET':
+	    form = UserCreationForm()
+	return render(request, 'accounts/signup.html', { 'form': form })
