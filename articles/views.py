95de1b5ae5800f66311e407de22fda56ed2f36f3
Alejandro <posdata.5@hotmail.com>
2019-01-05 17:58:58 -0600

    Added save function of article creation object to the DB

--- articles/views.py
+++ articles/views.py
@@ -19,7 +19,10 @@ def article_create(request):
 		form = forms.CreateArticle(request.POST, request.FILES)
 		if form.is_valid():
 			#Save article to DB
-			return redirect.article_list
+			instance = form.save(commit=False)
+			instance.author = request.user
+			instance.save()
+			return redirect('articles:list')
 	else:
 		form = forms.CreateArticle()
 	form = forms.CreateArticle()
